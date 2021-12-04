59e840881b5abfacbac0468cb68a9452c35ba118
jcaltamia <jayceealtamia@gmail.com>
2021-12-04 14:49:06 +0800

    remove comments

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,13 @@ const terser = require('gulp-terser');
 const cssnano = require('cssnano');
 const browsersync = require('browser-sync').create();
 
-
-//Sass Task
 function scssTask(){
-    //setting the source for the sass
+
     return src('app/scss/style.scss', {sourcemaps:true})
 
         .pipe(sass())
-         //minify css
         .pipe(postcss([cssnano()]))
-        //save to dist folder and save the sourcemaps in there
-        // . dot means save in the same location 
+  
         .pipe(dest('dist', {sourcemaps: '.'})); 
 
 }
@@ -26,14 +22,13 @@ function jsTask (){
         .pipe(dest('dist', {sourcemaps:'.'}));
 }
 
-//we need to use cb function because it is not a gulp plugin to tell the async we end  it we need explicitly  by that cb function 
+
 function browsersyncServe(cb){
     browsersync.init({
         server:{
             baseDir: '.'
         }
     }); 
-//to signify that it is complete
     cb();
 }
 
